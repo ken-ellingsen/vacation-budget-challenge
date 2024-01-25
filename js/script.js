05c49d84a67c259f279b65357b5d84bf69fde109
Ken Ellingsen <152531539+ken-ellingsen@users.noreply.github.com>
2024-01-25 14:17:39 -0600

    Added functionality to budget calculator button

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var tripInfoButton = document.querySelector(`.trip-info`);
+var dailyBudget = document.querySelector(`.daily-budget`);
+
+var tripInfo = function(){
+    var totalBudget = Number(prompt(`What's your total budget for this trip?`));
+    var accomodation = Number(prompt(`What are your accomodation costs?`));
+    var numDays = Number(prompt(`How many days is your trip?`));
+
+    calculateDailyBudget(totalBudget, accomodation, numDays);
+};
+
+var calculateDailyBudget = function(totalBudget, accomodation, numDays){
+    var daily = (totalBudget-accomodation)/numDays.toFixed(2);
+    dailyBudget.innerText = `You can spend $${daily} a day on food and fun.`
+};
+
+tripInfoButton.addEventListener(`click`, function(){
+    tripInfo();
+});
